c9a76422d25f9978e4a5be914c95c3057613fba5
ayushPahuja <ayushpahuja@makerstudio.io>
2024-07-01 12:55:06 +0530

    feat: add cookie and session middleware

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { Effect, Either } from "effect";
 import { addUser, getUsers } from "./services";
-
+import {WorkOS} from "@workos-inc/node"
+import { sealData, unsealData } from "iron-session";
+import { createRemoteJWKSet, jwtVerify } from "jose";
+import { sessionMiddleware, CookieStore } from "hono-sessions";
 
 const app = new Hono();
+
+const store = new CookieStore();
 
 const handleRequest = (path: string): Effect.Effect<Response, unknown, never> =>
   Either.try(() => {
@@ -14,6 +19,16 @@ const handleRequest = (path: string): Effect.Effect<Response, unknown, never> =>
       return new Response("Not Found", { status: 404 });
     }
   });
+  const handleRedirect = (
+    path: string
+  ): Effect.Effect<Response, unknown, never> =>
+    Either.try(() => {
+      if (path === "/login") {
+        return new Response("Login Successful!", { status: 200 });
+      } else {
+        return new Response("Failed to Login", { status: 404 });
+      }
+    });
 
 
 app.get("/hello", async (c) => {
@@ -46,6 +61,24 @@ app.post("/users",async(c)=> {
         return new Response("Error adding user", {status:500})
     }
 });
+
+const workos = new WorkOS(process.env.WORKOS_API_KEY as string);
+const clientId = process.env.WORKOS_CLIENT_ID as string;
+const cookiePassword = process.env.COOKIE_PASSWORD as string;
+
+app.use(
+  "*",
+  sessionMiddleware({
+    store,
+    encryptionKey: "password_at_least_32_characters_long",
+    expireAfterSeconds: 900,
+    cookieOptions: {
+      sameSite: "Lax",
+      path: "/auth",
+      httpOnly: true,
+    },
+  })
+);
 
 const port = 3000;
 console.log(`Server is running on port ${port}`);
